030ab53851ce1b762c2751a3b6e807904cd8c67a
Chris Dawes <cmsd2@cantab.net>
2017-03-04 11:43:26 +0000

    add service collection extension

---
+++ src/AspNetCore.Identity.DynamoDB/DynamoDBBuilder.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCore.Identity.DynamoDB
+{
+    public class DynamoDBIdentityBuilder<TUser,TRole>
+        where TUser: DynamoIdentityUser 
+        where TRole: DynamoIdentityRole
+    {
+        public IServiceCollection Services { get; set; }
+        public Type UserType { get; set; }
+        public Type RoleType { get; set; }
+
+        public DynamoDBIdentityBuilder(IServiceCollection services)
+        {
+            Services = services;
+            UserType = typeof(TUser);
+            RoleType = typeof(TRole);
+        }
+
+        private DynamoDBIdentityBuilder<TUser,TRole> AddScoped(Type serviceType, Type concreteType)
+        {
+            Services.AddScoped(serviceType, concreteType);
+            return this;
+        }
+
+        private DynamoDBIdentityBuilder<TUser,TRole> AddSingleton(Type serviceType, Type concreteType)
+        {
+            Services.AddSingleton(serviceType, concreteType);
+            return this;
+        }
+
+        public DynamoDBIdentityBuilder<TUser,TRole> AddUserStore<T>() where T: class
+        {
+            return AddSingleton(typeof(IUserStore<>).MakeGenericType(UserType), typeof(T));
+        }
+
+        public DynamoDBIdentityBuilder<TUser, TRole> AddUserStore()
+        {
+            return AddUserStore<DynamoUserStore<TUser, TRole>>();
+        }
+
+        public DynamoDBIdentityBuilder<TUser, TRole> AddRoleStore<T>() where T : class
+        {
+            return AddSingleton(typeof(IRoleClaimStore<>).MakeGenericType(RoleType), typeof(T));
+        }
+
+        public DynamoDBIdentityBuilder<TUser, TRole> AddRoleStore()
+        {
+            return AddRoleStore<DynamoRoleStore<TRole>>();
+        }
+
+        public DynamoDBIdentityBuilder<TUser, TRole> AddRoleUsersStore<T>() where T : class
+        {
+            return AddSingleton(typeof(T), typeof(T));
+        }
+
+        public DynamoDBIdentityBuilder<TUser, TRole> AddRoleUsersStore()
+        {
+            return AddRoleUsersStore<DynamoRoleUsersStore<TRole, TUser>>();
+        }
+    }
+}
---
+++ src/AspNetCore.Identity.DynamoDB/DynamoDBServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCore.Identity.DynamoDB
+{
+    public static class DynamoDBServiceCollectionExtensions
+    {
+        public static DynamoDBIdentityBuilder<TUser,TRole> AddDynamoDBIdentity<TUser, TRole>(this IServiceCollection services)
+            where TUser: DynamoIdentityUser
+            where TRole: DynamoIdentityRole
+        {
+            return new DynamoDBIdentityBuilder<TUser,TRole>(services);
+        }
+    }
+}
